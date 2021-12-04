cd3705aaf51f1dcf8e63aa36a65f823830218477
Truls Henrik Jakobsen <trulshj@stud.ntnu.no>
2021-12-04 15:26:29 +0100

    Create Python template for following days

---
+++ Python/dayXX.py
@@ -0,0 +1,16 @@
+
+data = [x.rstrip() for x in open("inputXX.txt").readlines()]
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+if __name__ == "__main__":
+    print("--- AOC Day XX ---")
+    print("Part 1:", part1(data))
+    print("Part 2:", part2(data))
